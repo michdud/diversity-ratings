4be6b563b932d6f41dcf5d0fbfca4d8dce25de17
michdud <mcdudley@knox.edu>
2017-04-08 11:33:16 -0500

    Update movie.js

--- js/movie.js
+++ js/movie.js
@@ -1,5 +1,6 @@
 $("#race_rating").click(function () {
   var reviewText = $('#race_explanation').val();
   $("#container").append('<div class="row">'+ reviewText +'</div>');
+  clearContents(race_explanation);
   console.log(hi);
 });
